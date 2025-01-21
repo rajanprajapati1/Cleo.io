adf46b7fe0325c7fc8f974a6459e21442c1cc848
RAJAN PRAJAPATI <113326646+rajanprajapati1@users.noreply.github.com>
2025-01-21 17:42:00 +0000

    Update Cleo.io.js

--- src/utils/Cleo.io.js
+++ src/utils/Cleo.io.js
@@ -1,7 +1,7 @@
 import Groq from "groq-sdk";
 
 const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY || `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`,
+  apiKey: process.env.GROQ_API_KEY || ``,
 });
 
 export const reqGroqAI = async (content) => {
